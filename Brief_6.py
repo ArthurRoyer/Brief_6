e845cee22bc5f29d07135d1b1e054891bca8238c
Arthur Royer <arthur.royer92@orange.fr>
2024-10-04 14:48:27 +0200

    Go

--- Brief_6.py
+++ Brief_6.py
@@ -47,7 +47,7 @@ CREATE TABLE IF NOT EXISTS "Commandes" (
 
 # Lecture et importation du fichier CSV des clients
 try:
-    clients_df = pd.read_csv('csv_clients.csv)
+    clients_df = pd.read_csv('csv_clients.csv')
     clients_df.to_sql('Client', conn, if_exists='replace', index=False)  # Remplace la table si elle existe
     print("Les données des clients ont été importées avec succès.")
 except Exception as e:
@@ -57,13 +57,57 @@ except Exception as e:
 # Lecture et importation du fichier CSV des commandes
 try:
     commandes_df = pd.read_csv('csv_commandes.csv')
-    commandes_df.to_sql('Commande', conn, if_exists='replace', index=False)  # Remplace la table si elle existe
+    commandes_df.to_sql('Commandes', conn, if_exists='replace', index=False)  # Remplace la table si elle existe
     print("Les données des commandes ont été importées avec succès.")
 except Exception as e:
     print(f"Erreur lors de l'importation des commandes : {e}")
 
+conn.commit()
 
+
+# Extraction des clients ayant consenti à recevoir des communications marketing
+print("Les clients ayant consenti à recevoir des communications marketing sont :")
 cursor.execute("SELECT * FROM Client WHERE Consentement_Marketing = 1")
+clients = cursor.fetchall()
+for client in clients: # Afficher les résultats
+    print(client[0])
+
+print("-----------------------------------------------------")
+print("-----------------------------------------------------")
+
+
+# Les commandes d'un client spécifique
+print("Les commandes du client avec comme id 23 sont :")
+cursor.execute("SELECT * FROM Commandes WHERE Client_ID = 23")
+clients = cursor.fetchall()
+for client in clients:
+    print(client)
+
+print("-----------------------------------------------------")
+print("-----------------------------------------------------")
+
+
+# Extraction des ID des clients ayant passé des cmmandes de plus de 100 euros
+print("Les clients ayant passé des commandes de plus de 100 euros sont :")
+cursor.execute("SELECT Client_ID FROM Commandes GROUP BY Client_ID HAVING SUM(Montant_Commande) >= 100")
+clients = cursor.fetchall()
+for client in clients:
+    print(client[0])
+
+print("-----------------------------------------------------")
+print("-----------------------------------------------------")
+
+
+# Extraction des ID des clients ayant passé des cmmandes après le 01/01/2023
+print("Les clients ayant passé des commandes après le 01/01/2023 sont :")
+cursor.execute("SELECT DISTINCT Client_ID FROM Commandes WHERE Date_Commande >= '2023-01-01'ORDER BY Client_ID ASC")
+clients = cursor.fetchall()
+for client in clients:
+    print(client[0])
+
+print("-----------------------------------------------------")
+print("-----------------------------------------------------")
+
 
 # Fermeture de la connexion
 conn.close()
